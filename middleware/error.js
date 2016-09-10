13f75d1b281457ac5ee1d6c0972d28dc117c1b2f
cesine <cesine@yahoo.com>
2016-09-10 15:49:46 -0400

    added error middleware

---
+++ middleware/error.js
@@ -0,0 +1,40 @@
+'use strict';
+
+var debug = require('debug')('middleware:error');
+
+/*jshint -W098 */
+function errors(err, req, res, next) {
+  /*jshint +W098 */
+  var data;
+
+  debug('in the error handler', err, err.stack);
+
+  if (process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'test') {
+    // expose stack traces
+    data = {
+      message: err.message,
+      error: err
+    };
+  } else {
+    // production error handler
+    data = {
+      message: err.message,
+      error: {}
+    };
+  }
+
+  data.status = err.status || err.statusCode || 500;
+
+  if (data.status === 500 && data.message === 'Failed to obtain access token') {
+    data.status = 403;
+  }
+
+  res.status(data.status);
+  if (typeof res.json === 'function'){
+    res.json(data);
+  } else if (typeof req.json === 'function'){
+    req.json(data);
+  }
+}
+
+module.exports = errors;
