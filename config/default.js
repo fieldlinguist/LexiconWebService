84dce441f81f5e596c1477325a14cf04cffee1bc
cesine <cesine@yahoo.com>
2016-09-11 19:16:50 -0400

    localport on elasticsearch is 9200

--- config/default.js
+++ config/default.js
@@ -2,5 +2,5 @@ var url = require("url");
 
 module.exports = {
   corpusOptions: url.parse("http://public:none@localhost:5984"),
-  searchOptions: url.parse("http://admin:none@localhost:3195/public/data")
+  searchOptions: url.parse("http://admin:none@localhost:9200/public/data")
 };
