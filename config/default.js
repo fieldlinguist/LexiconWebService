b88385763f5b52660784667ce5c80bff8881a994
cesine <cesine@yahoo.com>
2017-03-12 10:39:49 -0400

    merged config into default and using local.js to determine which service to connect if not the defaults

--- config/default.js
+++ config/default.js
@@ -1,8 +1,15 @@
-module.exports = {
+var fs = require("fs");
+
+var config = {
   corpus: {
-    url: process.env.FIELDDB_DATABASE_URL || "http://public:none@localhost:5984"
+    url: "http://public:none@localhost:5984"
   },
   search: {
-    url: process.env.FIELDDB_SEARCH_URL || "http://admin:none@localhost:9200"
+    url: "http://admin:none@localhost:9200"
+  },
+  url: "https://localhost:3185",
+  ssl: {
+    key: fs.readFileSync(__dirname + "/fielddb_debug.key", "utf8"),
+    cert: fs.readFileSync(__dirname + "/fielddb_debug.crt", "utf8")
   }
 };
--- config/development.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  corpus: {
-    url: "https://public:none@corpusdev.lingsync.org"
-  },
-  search: {
-    url: "https://searchdev.lingsync.org"
-  }
-};
